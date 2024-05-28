ad3a7cfe5777a8657adf730f07ecca16e537fdcc
kirankumargonti <kirankumar225rk@gmail.com>
2024-05-28 21:31:43 +0530

    fix: update the redirect url

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import {FC} from 'react'
 import {Provider} from 'react-redux'
+import {Auth0Provider} from '@auth0/auth0-react'
 
 import {store} from './Redux/store'
 
 import ThemeWrapper from './Config/ThemeWrapper'
 // Hoc
 import ErrorBoundary from './HOC/ErrorBoundary'
-import {Auth0Provider} from '@auth0/auth0-react'
 
 const App: FC = () => {
   return (
@@ -14,7 +14,7 @@ const App: FC = () => {
       domain={process.env.REACT_APP_DOMAIN_ID as string}
       clientId={process.env.REACT_APP_CLIENT_ID as string}
       authorizationParams={{
-        redirect_uri: `${window.location.origin}/profile`,
+        redirect_uri: process.env.REACT_APP_CALLBACK_URL as string,
       }}
     >
       <ErrorBoundary>
